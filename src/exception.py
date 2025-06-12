ae3ccfd9e00e4b8ed8298485399d9c5271cceeb5
santhosh <santhoshkommula98@gmail.com>
2025-06-12 11:40:07 +0530

    exception and logger setup done

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -15,3 +16,10 @@ class CustomException(Exception):
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     def __str__(self):
         return self.error_message
+    
+if  __name__=="__main__":
+    try:
+        a=10/0
+    except Exception as e:
+        logging.info('division by 0')
+        raise CustomException(e,sys)
